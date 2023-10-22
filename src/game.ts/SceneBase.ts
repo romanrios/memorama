7bd8d0e4b7332019b9a73f45d1b863fca43374b2
Roman Rios <romanrios@live.com>
2023-10-22 00:53:17 -0300

    gh-pages

--- src/game.ts/SceneBase.ts
+++
@@ -1,5 +0,0 @@
-import { Container } from "pixi.js";
-
-export abstract class SceneBase extends Container{
-    public abstract  update(deltaFrame:number,deltaTime?:number):void
-}
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-
-import { loaderScene } from "./game.ts/loaderScene";
-import { SceneManager } from "./game.ts/SceneManager";
-
-
-
-SceneManager.initialize();
-SceneManager.changeScene(new loaderScene );
